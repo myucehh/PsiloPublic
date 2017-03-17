8061183a4daf896cbe3b26d31f29fa58fb2b399f
myucehh <blingblong336@gmail.com>
2017-03-17 12:07:14 -0700

    added Anothony's chracter scene

---
+++ Psilo Tests/From Anthony/Psilo Adventure/Assets/Scripts/OmniCamera.cs
@@ -0,0 +1,92 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof (PlayerInput))]
+public class OmniCamera : MonoBehaviour {
+
+    // Camera script is to be attached to the Main Camera.  
+    // A PlayerInput script is required for controlling rotation and such.  
+    
+        // Initial camera placement is determined in the Inspector.
+    // This script governs how the camera moves and rotates once play has begin, based on where it was initially placed. 
+
+
+    public enum CameraType { Static, Follow, RotateAround, FirstPerson, SelfieMode };   //sets the options available for list of camera types
+    public CameraType cameraType;   //sets up a variable to be set in the Inspector, using options for camera types
+    string cameraoption;            //sets up a string to hold the camera type selected in the inspector
+    Transform target;
+    Vector3 cameraToPlayerVector;
+    Vector3 rotationVector;
+    public float cameraSpeed = 1;   //for smoothest results, the camera flies through space at its own speed
+    PlayerInput playerInput;
+    public float cameraFocus = 2f;  // how high above the characters head is the camera aimed? 
+
+    private void Start()
+    {
+        target = GameObject.FindGameObjectWithTag("Player").transform;
+        cameraoption = cameraType.ToString();       //converts camera type to a string, making it easier for me to work with
+        cameraToPlayerVector = transform.position - target.position;
+        rotationVector = cameraToPlayerVector;
+        playerInput = GetComponent<PlayerInput>();
+    }
+
+    void Update()
+    {
+        cameraoption = cameraType.ToString();       //command repeats in update so camera type can be changed during playtesting
+    }
+
+    
+    
+
+    void LateUpdate()
+    {
+
+        if (cameraoption == "SelfieMode")
+        {
+            float rotateMotion = playerInput.GetMouseHorizontalInput() * cameraSpeed;
+            target.transform.Rotate(0, rotateMotion, 0); 
+            float desiredAngle = target.transform.eulerAngles.y;
+            Quaternion rotation = Quaternion.Euler(0, desiredAngle, 0);
+            transform.position = target.transform.position - (rotation * cameraToPlayerVector);
+        }
+
+        if (cameraoption == "Follow")
+        {
+            if (playerInput.GetMouseMoveStatus() == true)
+                { target.transform.Rotate(0, (playerInput.GetMouseHorizontalInput() * cameraSpeed), 0); }
+            float desiredAngle = target.transform.eulerAngles.y;
+            Quaternion rotation = Quaternion.Euler(0, desiredAngle, 0);
+            transform.position = target.transform.position + (rotation * cameraToPlayerVector);
+        }
+   
+
+
+        if (cameraoption == "RotateAround")
+        {
+            rotationVector = Quaternion.AngleAxis(playerInput.GetMouseHorizontalInput() * cameraSpeed, Vector3.up) * rotationVector;
+            transform.position = target.position + rotationVector;
+
+        }
+        if (cameraoption == "FirstPerson")
+        {
+            if (playerInput.GetMouseMoveStatus() == true)
+            { target.transform.Rotate(0, (playerInput.GetMouseHorizontalInput() * cameraSpeed), 0); }
+            transform.position = new Vector3(target.position.x, target.position.y + 1.5f, target.position.z + 0.6f);
+            float desiredAngle = target.transform.eulerAngles.y;
+            Quaternion rotation = Quaternion.Euler(0, desiredAngle, 0);
+            transform.position = target.transform.position + (rotation * (transform.position-target.position));
+            transform.eulerAngles = new Vector3(transform.eulerAngles.x, target.eulerAngles.y, transform.eulerAngles.z);
+        }
+        else transform.LookAt(new Vector3(target.position.x, target.position.y+cameraFocus, target.position.z));
+
+
+        
+        
+        
+
+    }
+
+
+    
+}
---
+++ Psilo Tests/From Anthony/Psilo Adventure/Assets/Scripts/PlayerInput.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerInput : MonoBehaviour {
+
+    // This script gets information about the current user input with the mouse and keyboard.  It makes this information
+    // available for other scripts to get during its update process.  This allows you to easily change key configurations, 
+    // configure different controller types, add local multiplayer options, and otherwise change or simulate player input
+    // easily without modifying other scripts. 
+    // This script should go on every object that the player has control over, such as the player character and camera. 
+
+
+    float inputVertical;
+    float inputHorizontal;
+    float inputMouseHorizontal;
+    float inputMouseVertical;
+    string inputAction;
+    bool mousemove;
+	
+	void Update ()
+    {
+        ResetInputValues();
+        inputVertical = Input.GetAxis("Vertical");
+        if (Input.GetMouseButton(2)) { mousemove = true;  inputVertical = 1.0f; } else { mousemove = false; }
+        inputHorizontal = Input.GetAxis("Horizontal");
+        if (Input.GetKeyDown(KeyCode.Space)) { inputAction = "Jump"; }
+        if (Input.GetMouseButtonDown(0)) { inputAction = "Jump"; }
+        if (Input.GetKeyDown(KeyCode.Return)) { inputAction = "Toss"; }
+        if (Input.GetMouseButtonDown(1)) { inputAction = "Toss"; }
+        inputMouseHorizontal = Input.GetAxis("Mouse X");
+        inputMouseVertical = Input.GetAxis("Mouse Y");
+    }
+
+    void ResetInputValues()
+    {
+        //Shared variables for buttons presses need to have values reset. Floats from Horizontal and Vertical "GetAxis" don't. 
+        inputAction = "";
+    }
+
+    public float GetVerticalInput()
+    { return inputVertical; }
+
+    public float GetHorizontalInput()
+    { return inputHorizontal; }
+
+    public float GetMouseVerticalInput()
+    { return inputMouseVertical; }
+
+    public float GetMouseHorizontalInput()
+    { return inputMouseHorizontal; }
+
+    public string GetActionInput()
+    { return inputAction; }
+
+    public bool GetMouseMoveStatus()
+    { return mousemove; }
+
+
+
+}
